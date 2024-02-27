04c703c7b4815ae9d06a15bd52184a615dce1032
Zeeshan Khan <142289960+Zeeshankhan02@users.noreply.github.com>
2024-02-27 19:40:31 +0530

    Update index.js

--- server-side/index.js
+++ server-side/index.js
@@ -6,41 +6,45 @@ const cors = require("cors");
 const app = express();
 app.use(express.json());
 
-app.use(cors(
-  {
-    origin: `http://localhost:5173`,
-    methods: ["POST", "GET"],
-    credentials: true
-  }
-));
+// CORS middleware to handle preflight requests
+app.options('*', cors());
+
+app.use(cors({
+  origin: "*", // Allowing requests from any origin
+  allowedHeaders: "*",
+  allowedMethods: "*",
+  credentials: true
+}));
+
 mongoose.connect(
-  "mongodb+srv://ZeeshanKhan:Zxcvbnm@cluster0.jxlmfoz.mongodb.net/NewsHub?retryWrites=true&w=majority&appName=Cluster0"
+  "mongodb+srv://ZeeshanKhan:Zxcvbnm@cluster0.jxlmfoz.mongodb.net/NewsHub?retryWrites=true&w=majority&appName=Cluster0",
+  { useNewUrlParser: true, useUnifiedTopology: true }
 );
 
 app.post(`/si`, (req, res) => {
   UserModel.create(req.body)
-    
-    .then((users) => res.json(users))
-    .catch((err) => res.json(err));
+    .then((user) => res.json(user))
+    .catch((err) => res.status(400).json(err));
 });
 
 app.post(`/logIn`, (req, res) => {
   const { email, password } = req.body;
   UserModel.findOne({ email: email })
-  .then((user) => {
-    if (user) {
-      if (user.password === password) {
-        res.json("Success");
+    .then((user) => {
+      if (user) {
+        if (user.password === password) {
+          res.json("Success");
+        } else {
+          res.json("Incorrect Password");
+        }
       } else {
-        res.json("Incorrect Password");
+        res.json("User Not Found");
       }
-    } else {
-      res.json("User Not Found");
-     
-    }
-  });
+    })
+    .catch((err) => res.status(400).json(err));
 });
 
-app.listen(3002, () => {
-  console.log("server is running");
+const PORT = process.env.PORT || 3002;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
