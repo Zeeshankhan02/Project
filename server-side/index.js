dd4099ff291c3f0287bdf400ac28d8f1c73c994e
Zeeshan Khan <142289960+Zeeshankhan02@users.noreply.github.com>
2024-02-27 19:08:16 +0530

    Update index.js

--- server-side/index.js
+++ server-side/index.js
@@ -5,6 +5,11 @@ const cors = require("cors");
 
 const app = express();
 app.use(express.json());
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+  });
 app.use(cors(
   {
     origin:  'http://localhost:5173',
