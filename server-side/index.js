5ad26b4c2b825c2c535061e2b2c243947ac3fc97
Zeeshan Khan <142289960+Zeeshankhan02@users.noreply.github.com>
2024-02-27 17:49:42 +0530

    Update index.js

--- server-side/index.js
+++ server-side/index.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(express.json());
 app.use(cors(
   {
-    origin: ["http://localhost:5173"],
+    origin: ['https://client-side-woad.vercel.app'],
     methods: ["POST", "GET"],
     credentials: true
   }
