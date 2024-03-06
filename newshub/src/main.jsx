23f3498c179634b0abdfaa42ec0efd5466155743
Zeeshan khan <zeeshankhan0271@gmail.com>
2024-03-06 19:09:02 +0530

    changed api

--- newshub/src/main.jsx
+++ newshub/src/main.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-// import App from './App.jsx'
-import Home from './website/Home.jsx'
+import App from './App.jsx'
+// import Home from './website/Home.jsx'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Home />
+    <App />
   </React.StrictMode>
 )
